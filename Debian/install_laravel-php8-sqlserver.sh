e73741a684a274e093b2d8ffd3a5739ff8daae12
Mateus Galasso <mateusgalasso@yahoo.com.br>
2021-04-29 09:21:51 -0300

    Update install_laravel-php8-sqlserver.sh

--- Debian/install_laravel-php8-sqlserver.sh
+++ Debian/install_laravel-php8-sqlserver.sh
@@ -5,37 +5,52 @@
 ###########################################################
 #  'Update and Upgrade'
 apt update && apt upgrade -y
+
 #  'instala uns programas básicos'
 apt -y install lsb-release apt-transport-https ca-certificates wget redis-server nginx unzip libcurl4-openssl-dev
+
 #  'adicliona mais lista de pacotes'
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+
 #  'update novamente'
 apt update
+
 #  'instala php 8'
 apt upgrade -y && apt -y install php
+
 #  'instala extensions'
-apt -y install curl php-mbstring git unzip php8.0-{common,mysql,xml,xmlrpc,curl,gd,imagick,cli,dev,imap,mbstring,opcache,soap,zip,intl,bcmath,ldap,sqlite}
+apt -y install curl php-mbstring git unzip php8.0-{common,mysql,xml,xmlrpc,curl,gd,imagick,cli,dev,imap,mbstring,opcache,soap,zip,intl,bcmath,ldap,sqlite,fpm}
+
 #  'more php' 
 apt-get -y install php8.0-sqlite php-xml php-xml wrk unixodbc-dev supervisor
-# wrk
+
 #  'COMPOSER'
 sh -c "echo 'precedence ::ffff:0:0/96 100' >> /etc/gai.conf"
 wget -O composer-setup.php https://getcomposer.org/installer
 php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
 # Instala sqlserver drives
-# apt-get install curl apt-transport-https
-# wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-# echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
-# apt-get update
-# sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
-# locale-gen
-# pecl install sqlsrv
-# pecl install pdo_sqlsrv
-# phpenmod sqlsrv pdo_sqlsrv
-# # adiciona sqlserver drives nas extensions do php
-# bash -c 'printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.0/mods-available/sqlsrv.ini'
-# bash -c 'printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.0/mods-available/pdo_sqlsrv.ini'
+apt-get install curl apt-transport-https
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+apt-get update
+sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+pecl install sqlsrv
+pecl install pdo_sqlsrv
+phpenmod sqlsrv pdo_sqlsrv
+# adiciona sqlserver drives nas extensions do php
+bash -c 'printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.0/mods-available/sqlsrv.ini'
+bash -c 'printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.0/mods-available/pdo_sqlsrv.ini'
+
+# Instala Redis
+service redis-server start
+# Instala Laravel
+cd /var/www/
+composer create-project --no-interaction --prefer-dist laravel/laravel laravel
+cd laravel
+
 # Instala Swoole
 pecl install -D 'enable-sockets="no" enable-openssl="yes" enable-http2="yes" enable-mysqlnd="yes" enable-swoole-json="no" enable-swoole-curl="yes"' swoole
 
@@ -43,95 +58,43 @@ pecl install -D 'enable-sockets="no" enable-openssl="yes" enable-http2="yes" ena
 # php -i | grep php.ini
 echo "extension=swoole">/etc/php/8.0/cli/conf.d/20-swoole.ini
 # php -m | grep swoole
-# Instala Redis
-service redis-server start
-# Instala Laravel
-cd /var/www/
-composer create-project --no-interaction --prefer-dist laravel/laravel laravel
-cd laravel 
-# Instala octane
-composer require laravel/octane -n
-php artisan octane:install --server=swoole
+composer require swooletw/laravel-swoole -n
+
 # da permissoes
 chown -R :www-data /var/www/laravel/storage/
 chown -R :www-data /var/www/laravel/bootstrap/cache/
 chmod -R 0777 /var/www/laravel/storage/
-chmod -R 0777 /var/www/laravel/bootstrap/cache/
-chmod -R 0777 /var/www/laravel/artisan
+chmod -R 0775 /var/www/laravel/bootstrap/cache/
 chown -R www-data.www-data /var/www/laravel/storage
 chown -R www-data.www-data /var/www/laravel/bootstrap/cache
 mkdir -p /var/www/laravel
 
 # Configura nginx
+service nginx stop
 rm /etc/nginx/sites-available/default
 rm /etc/nginx/sites-enabled/default
 rm -rf /var/www/html
-# cp /var/www/laravel /etc/nginx/sites-available/laravel -r
-bash -c 'printf "
-map $http_upgrade $connection_upgrade {
-    default upgrade;
-    ''      close;
-}
-server {
-    listen 80;
-    server_name your.domain.com;
-    root /var/www/laravel/public;
-    index index.php;
+cp /home/mateus/laravel-swoole.conf /etc/nginx/sites-available/laravel.conf
+ln -s /etc/nginx/sites-available/laravel.conf /etc/nginx/sites-enabled/
 
-    location = /index.php {
-        # Ensure that there is no such file named "not_exists"
-        # in your "public" directory.
-        try_files /not_exists @swoole;
-    }
-    # any php files must not be accessed
-    #location ~* \.php$ {
-    #    return 404;
-    #}
-    location / {
-        try_files $uri $uri/ @swoole;
-    }
-
-    location @swoole {
-        set $suffix "";
-
-        if ($uri = /index.php) {
-            set $suffix ?$query_string;
-        }
-
-        proxy_http_version 1.1;
-        proxy_set_header Host $http_host;
-        proxy_set_header Scheme $scheme;
-        proxy_set_header SERVER_PORT $server_port;
-        proxy_set_header REMOTE_ADDR $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection $connection_upgrade;
-
-        # IF https
-        # proxy_set_header HTTPS "on";
-
-        proxy_pass http://127.0.0.1:8000$suffix;
-    }
-}
-" > /etc/nginx/sites-available/laravel.conf'
-ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/
-service nginx restart
 # Instalando e criando supervisor para laravel
 apt-get install supervisor
-# Octane Supervisor
-sudo bash -c 'printf "[program:laravel-octane]
-process_name=laravel_octane_02d
-command=php /var/www/larave/artisan octane:start
-autostart=true
+
+# laravel-swoole
+bash -c 'printf "[program:laravel-swoole]
+process_name=laravel_swoole
+command=php /var/www/laravel/artisan swoole:http start
+autorestart=true
 autorestart=true
 user=root
 numprocs=1
 redirect_stderr=true
-stdout_logfile=/var/www/laravel/storage/logs/octane.log" > /etc/supervisor/conf.d/laravel-octane.conf'
-# Queue Superficos
+stdout_logfile=/var/www/laravel/storage/logs/laravel-swoole.log" > /etc/supervisor/conf.d/laravel-swoole.conf'
+
+# Queue workers
 bash -c 'printf "[program:laravel-worker]
-process_name=laravel_worker_02d
-command=php /var/www/larave/artisan queue:work --sleep=3 --tries=3
+process_name=laravel_worker
+command=php /var/www/laravel/artisan queue:work --sleep=3 --tries=3
 autostart=true
 autorestart=true
 user=root
@@ -139,9 +102,32 @@ numprocs=1
 redirect_stderr=true
 stdout_logfile=/var/www/laravel/storage/logs/worker.log
 stopwaitsecs=3600" > /etc/supervisor/conf.d/laravel-worker.conf'
-service supervisor start
+
 # Lembrar de alterar o usuário no final do comando
 bash -c 'echo "* * * * * /var/www/laravel && php artisan schedule:run >> /dev/null 2>&1" >>  /var/spool/cron/crontabs/mateus'
 service cron start
 apt autoremove -y
+
+# Adiciona algumas coisas no supervisor
+sudo touch /var/run/supervisor.sock
+sudo chmod 777 /var/run/supervisor.sock
+echo_supervisord_conf > /etc/supervisord.conf
+sudo supervisord -c /etc/supervisord.conf
+
+# # Instala systemctl para o wls2
+# git clone https://github.com/DamionGans/ubuntu-wsl2-systemd-script.git
+# cd ubuntu-wsl2-systemd-script/
+# bash ubuntu-wsl2-systemd-script.sh
+
+# Inicia servicos
+# sudo systemctl start supervisor
+# sudo systemctl enable supervisor
+sudo service supervisor start
+sudo update-rc.d supervisor defaults
+sudo update-rc.d supervisor enable
+# sudo systemctl start nginx
+# sudo systemctl enable nginx
+sudo service nginx start
+sudo update-rc.d nginx defaults
+sudo update-rc.d nginx enable
 exit
